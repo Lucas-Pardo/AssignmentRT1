5990fa47d90281c0b91c0af44dde16e1b9529a7b
Lucas-Pardo <lucas.pardo@hotmail.com>
2022-11-08 18:33:13 +0100

    try fix1

--- Project/assignment.py
+++ Project/assignment.py
@@ -1,6 +1,9 @@
 from __future__ import print_function
 import time
+from timeit import default_timer
+from math import sqrt
 from sr.robot import *
+
 
 a_th = 2.0
 """ float: Threshold for the control of the orientation"""
@@ -9,10 +12,13 @@ d_th = 0.4
 """ float: Threshold for the control of the linear distance"""
 
 pd_th = 0.6
-""" float: Distance at which leave a token to pair to another"""
+""" float: Threshold for the control of the release distance"""
 
-in_time = 15.0
-""" float: Time in seconds of inactivity to consider all tokens paired"""
+dt = 0.02
+""" float: Time step used in all motion functions"""
+
+grab_index = 0
+""" int (0, 1): Type of token to grab. 0 for silver, 1 for gold"""
 
 R = Robot()
 """ instance of the class Robot"""
@@ -83,8 +89,8 @@ def search_and_grab(exc_list=[[], []], dt=0.4, marker_type=MARKER_TOKEN_SILVER):
         d, ang, cd = find_free_token(marker_type, exc_list)
         if d < 0:
             return -1
-        speed = d * 30
-        ang_speed = ang / 2
+        speed = d * (1 + round(16 / dt))
+        ang_speed = min(ang / 4 / dt, 3 * ang)
         if d > d_th:
             drive(speed, dt)
         if abs(ang) > a_th:
@@ -106,8 +112,8 @@ def search_and_release(exc_list=[[], []], dt=0.4, marker_type=MARKER_TOKEN_GOLD)
         d, ang, cd = find_free_token(marker_type, exc_list)
         if d < 0:
             return -1
-        speed = d * 30
-        ang_speed = ang / 2
+        speed = d * (1 + round(16 / dt))
+        ang_speed = min(ang / 4 / dt, 3 * ang)
         if d > pd_th:
             drive(speed, dt)
         if abs(ang) > a_th:
@@ -115,39 +121,44 @@ def search_and_release(exc_list=[[], []], dt=0.4, marker_type=MARKER_TOKEN_GOLD)
         if (d <= pd_th) and (abs(ang) <= a_th):
             R.release()
             return cd
-
-
-# Modifiable parameters:
-dt = 0.4
-speed = 15
-grab_index = 0
+        
+        
+def main():
+    t = 0
+    in_time = 14 * sqrt(dt)
+    speed = 1 + round(17 / sqrt(dt))
+    grab_state = False
+    markers = [MARKER_TOKEN_SILVER, MARKER_TOKEN_GOLD]
+    release_index = (grab_index + 1) % 2
+    arranged = [[], []]
+    rot = 1
+    while t < in_time:
+        if not grab_state:
+            cd = search_and_grab(arranged, dt, markers[grab_index])
+            if cd == -1:
+                turn(rot * speed, dt)
+                t += dt
+            else:
+                grab_state = True
+                arranged[grab_index].append(cd)
+                t = 0
+                rot *= -1
+        else:
+            cd = search_and_release(arranged, dt, markers[release_index])
+            if cd == -1:
+                turn(rot * speed, dt)
+            else:
+                drive(-speed, speed * dt / 6)
+                grab_state = False
+                arranged[release_index].append(cd)
+    return in_time
 
 # Main Code:
-t = 0
-grab_state = False
-markers = [MARKER_TOKEN_SILVER, MARKER_TOKEN_GOLD]
-release_index = (grab_index + 1) % 2
-arranged = [[], []]
-while t < in_time:
-    if not grab_state:
-        cd = search_and_grab(arranged, dt, markers[grab_index])
-        if cd == -1:
-            turn(speed, dt)
-            t += dt
-        else:
-            grab_state = True
-            arranged[grab_index].append(cd)
-            t = 0
-    else:
-        cd = search_and_release(arranged, dt, markers[release_index])
-        if cd == -1:
-            turn(speed, dt)
-        else:
-            drive(-speed*5, dt*2)
-            turn(speed, dt*2)
-            grab_state = False
-            arranged[release_index].append(cd)
-            
-print("Finished.")
+if __name__ == "__main__":                  #Check if the file is run as a script.
+    #execfile("run.py")                      #Execute run.py
+    start_time = default_timer()            #Start timer
+    in_time = main()                        #Run main function
+    ex_time = default_timer() - start_time  #End timer  
+    print("Finished in {:.3f} seconds with {:.1f} seconds of inactivity.".format(ex_time, in_time))
     
     
